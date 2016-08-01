c997a4f3aba010a1471d88a5381b484744a0e061
刘万兵 <970006421@qq.com>
2016-08-01 11:16:18 +0800

    版本更新#

---
+++ DJ哦啊阿佛打啊/DJ哦啊阿佛打啊/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJ哦啊阿佛打啊
+//
+//  Created by lanou on 16/8/1.
+//  Copyright © 2016年 刘万兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DJ哦啊阿佛打啊/DJ哦啊阿佛打啊/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  DJ哦啊阿佛打啊
+//
+//  Created by lanou on 16/8/1.
+//  Copyright © 2016年 刘万兵. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"兵哥好帅");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
